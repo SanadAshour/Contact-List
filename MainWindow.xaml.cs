0ba6d33b8bfa620164abbdbec799bbb59434a76e
Sanad Ashour <sanadashour2002@yahoo.com>
2025-07-31 15:53:40 +0300

    MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,10 +22,19 @@ namespace Contact
             ShowData();
         }
         private AppDbContext ADC = new AppDbContext();
+        private int selectedId = 0;
 
         private void ShowData()
         {
             list.ItemsSource = ADC.MyContacts.ToList();
+        }
+
+        private void ClearData()
+        {
+            fnameTB.Clear();
+            lnameTB.Clear();
+            PnumTB.Clear();
+            emailTB.Clear();
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
@@ -39,14 +48,53 @@ namespace Contact
                 gender = genderCB.Text,
             });
             ADC.SaveChanges();
-
-            fnameTB.Clear();
-            lnameTB.Clear();
-            PnumTB.Clear();
-            emailTB.Clear();
-
+            ClearData();
             ShowData();
             MessageBox.Show("ITEMS ADDED!", "CONFIRMATION", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (list.SelectedItem is MyContacts item)
+            {
+                fnameTB.Text = item.Fname;
+                lnameTB.Text = item.Lname;
+                PnumTB.Text = item.PhoneNumber?.ToString() ?? "";
+                emailTB.Text = item.email;
+                genderCB.Text = item.gender; 
+                selectedId = item.id;
+            }
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedId != 0)
+            {
+                var item = ADC.MyContacts.Find(selectedId);
+                item.Fname = fnameTB.Text;
+                item.Lname = lnameTB.Text;
+                item.PhoneNumber = int.Parse(PnumTB.Text);
+                item.email = emailTB.Text;
+                item.gender = genderCB.Text;
+                ADC.MyContacts.Update(item);
+                ADC.SaveChanges();
+                ClearData();
+                MessageBox.Show("ITEM EDITED!", "CONFIRMATION", MessageBoxButton.OK, MessageBoxImage.Information);
+                ShowData();
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedId != 0)
+            {
+                var item = ADC.MyContacts.Find(selectedId);
+                ADC.MyContacts.Remove(item);
+                ADC.SaveChanges();
+                ClearData();
+                MessageBox.Show("ITEM DELETED!", "CONFIRMATION", MessageBoxButton.OK, MessageBoxImage.Information);
+                ShowData();
+            }
         }
     }
 }
