ee925a02c86d9a1666356247d35254bc8a754e19
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-04 23:54:15 +0300

    AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +10,20 @@ namespace Contact
     internal class AppDbContext: DbContext
     {
         public DbSet<MyContacts> MyContacts { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=Sanad_HpEnvy\\SQLEXPRESS;database=Contacts;integrated security=true;TrustServerCertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.ConList) //a category has many todoitems
+                .WithOne(cl => cl.Category) //a todoitem has one category
+                .HasForeignKey(cl => cl.CategoryId); //the foreign key is in todoitem
         }
     }
 }
